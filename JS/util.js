121cfe70a459d0d4ed34d01fd261219a074e0d17
Alon Goddard <112505002+ALONGOD@users.noreply.github.com>
2024-11-05 10:57:41 +0200

    Update util.js

--- JS/util.js
+++ JS/util.js
@@ -6,6 +6,7 @@ function createCell(gameObject = null) {
         gameObject: gameObject
     }
 }
+
 function getElCell(pos) {
     return document.querySelector(`[data-i='${pos.i}'][data-j='${pos.j}']`)
 }
